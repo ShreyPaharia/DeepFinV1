9d26efe4b7ec36915385475389393113933b70d4
hack3r-0m <oparikh113@gmail.com>
2021-08-04 15:55:41 +0530

    new: super token deploy file

---
+++ packages/hardhat/deploy/03_deploy_super_deposit_token.js
@@ -0,0 +1,20 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    await deploy("SuperDepositToken", {
+        from: deployer,
+        log: true,
+      });
+
+    const SuperDepositToken = await ethers.getContract("SuperDepositToken", deployer);
+    await SuperDepositToken.initialize(
+        "Super Deposit Token",
+        "SDT",
+        "5000000000000000000000"
+    )
+
+    console.log(SuperDepositToken.balanceOf(SuperDepositToken.address))
+}
+
+module.exports.tags = ["SuperDepositToken"];
